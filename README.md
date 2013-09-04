8f8cb7be6014cffda3eb68dc457b8354a4cf572f
Fernando Briano <fernando@picandocodigo.net>
2013-09-04 15:41:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Konami code
+
+JavaScript implementation of the Konami Code:
+
+:arrow_up: :arrow_up: :arrow_down: :arrow_down: :arrow_left: :arrow_right: :arrow_left: :arrow_right: :b: :a:
---
+++ main.js
@@ -0,0 +1,30 @@
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var input = []
+
+document.onkeyup = function(key){
+  for(var i = 0; i < code.length; i++){
+    if (key.which == code[i]){
+      input.push(code[i]);
+      if (input[i] == code[i]){
+        if (arraysEqual(code, input)){
+          alert("Konami Code executed!");
+        }
+      }else{
+        input = [];
+      }
+      break;
+    }
+  }
+  return false;
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
